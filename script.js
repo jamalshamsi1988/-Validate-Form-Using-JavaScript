da1a21b2ecb5345d0771311182b626a8851c2774
jamalshamsi <jamal.shamsi1988@gmail.com>
2022-04-12 23:42:11 +0430

    form completed

--- script.js
+++ script.js
@@ -20,14 +20,49 @@ return true;
 
 function validatePhone() {
   let phone = document.getElementById("contact-phone").value;
-  if (phone.length == 0) {
+  if (phone.length !== 10) {
     phoneError.innerText = "Phone is required";
     return false;
   }
-  if (!phone.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
-    phoneError.innerText = "phone num shoud be 10 dihits";
+  if (!phone.match(/^[0-9]{10}$/)) {
+    phoneError.innerText = "Only digits please";
     return false;
   }
   phoneError.innerText = '<i class="fa-solid fa-circle-check"></i>';
   return true;
+}
+
+
+function validateEmail() {
+  let email = document.getElementById("contact-email").value;
+  if (email.length == 0) {
+    emailError.innerText = "Email is required";
+    return false;
+  }
+  if (!email.match(/^[A-Za-z]\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+    emailError.innerText = "Email Invalid";
+    return false;
+  }
+  emailError.innerText = '<i class="fa-solid fa-circle-check"></i>';
+  return true;
+}
+
+function validateMessage(){
+  let message = document.getElementById("contact-message").value;
+  let required=30;
+  let left=required - message.length;
+
+  if(left > 0){
+    messageError.innerText = left + "More characters required";
+    return false;
+  }
+  messageError.innerText = '<i class="fa-solid fa-circle-check"></i>';
+}
+
+function  validateForm(){
+  if (!validateName() || !validatePhone() || !validateEmail() || !validateMessage());
+  submitError.style.display="block";
+  submitError.innerText = "Pleas fix error to submit";
+  setTimeout(function(){submitError.style.display="none";},3000);
+  return false;
 }
